576627818d8ec0cd9cc62644386b85d1485b4d1a
ujon <ujon94@gmail.com>
2024-10-29 15:00:34 -0700

    feat: add Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,56 @@
+<script>
+	import LocaleSwitch from '$lib/components/LocaleSwitch.svelte';
+	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
+	import ContactLinks from '$lib/components/ContactLinks.svelte';
+</script>
+
+<footer class="container padding-x padding-y border-color-1">
+	<div class="content">
+		<LocaleSwitch />
+		<ThemeSwitch />
+	</div>
+	<div class="following">
+		<p class="copyright text-color-1">Copyright 2024 © ujon. All right reserved</p>
+		<ContactLinks />
+	</div>
+</footer>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+        border-top-width: 0.0625rem;
+
+        & > .content {
+            display: flex;
+            flex-direction: column;
+            gap: 1.25rem;
+        }
+
+        & > .following {
+            width: 100%;
+            display: flex;
+            flex-direction: column-reverse;
+            align-items: center;
+            gap: 1rem;
+
+            & > .copyright {
+                flex: 1 1 0;
+            }
+        }
+    }
+
+    @media (min-width: 1024px) {
+        .container {
+            align-items: start;
+
+            & > .following {
+                flex-direction: row;
+                align-items: end;
+            }
+        }
+    }
+</style>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
-	import "$styles/index.css"
+	import '$styles/index.css';
 	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Header />
 <slot />
+<Footer />
