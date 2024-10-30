14caf9677515cbeb1d68d3531603b3e82fabdd2b
ujon <ujon94@gmail.com>
2024-10-30 15:26:01 -0700

    feat: add Contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,31 @@
 <script>
-
+	import { t } from '$lib/translations';
+	import ContactLinks from '$lib/components/ContactLinks.svelte';
 </script>
 
+<section class="hero-container">
+	<p class="title text-display-3">{$t("contact.title")}</p>
+	<ContactLinks />
+</section>
+
+<style>
+    .hero-container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 1.75rem;
+
+        & > .title {
+            text-align: center;
+            white-space: pre-line;
+        }
+    }
+
+    @media (min-width: 1024px) {
+        .hero-container {
+            height: calc(100vh - var(--header-h));
+        }
+    }
+</style>
